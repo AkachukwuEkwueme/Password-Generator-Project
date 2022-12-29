5568410faed39cb3ed923e4dd4fd62cf0e0d91b6
AkachukwuEkwueme <121521135+AkachukwuEkwueme@users.noreply.github.com>
2022-12-28 23:26:04 -0500

    Add files via upload

--- Password.java
+++ Password.java
@@ -7,22 +7,27 @@ public class Password {
 
 public static void main(String[] args){
 
-    passwordRules();
+   // passwordRules();
+    String[] savedchoices = passwordRules();
+    int passlength= Integer.parseInt(savedchoices[3]);
+    String password = passwordGenerator(savedchoices[0],savedchoices[1],savedchoices[2],passlength);
+    System.out.println(password);
+    //System.out.println(passwordGenerator("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+", 15));
 
 }
-static void passwordRules() { //Method for instructing rules for password
+public static String[] passwordRules() { //Method for instructing rules for password
     Scanner myObj = new Scanner(System.in);
 
     System.out.println("\n Welcome to the MEDFetch App, for improving daily healthcare and making it more accessible");
     System.out.println("\n We will create you a password to keep your personal account secure");
     
-    System.out.println("\n Do you want to use Letters (Uppercase and Lower)? Indicated with(Y/N)");
+    System.out.println("\n Do you want to use Letters (Uppercase and Lower)? Indicate with(Y/N)");
     String firstchoice = myObj.nextLine();
     System.out.println("Use of Letters: " + firstchoice);
-    System.out.println("\n Do you want to use Numbers? Indicated with(Y/N)");
+    System.out.println("\n Do you want to use Numbers? Indicate with(Y/N)");
     String secondchoice = myObj.nextLine();
     System.out.println("Use of Numbers: " + secondchoice);
-    System.out.println("\n Do you want to use Characters? Indicated with(Y/N)");
+    System.out.println("\n Do you want to use Characters? Indicate with(Y/N)");
     String thirdchoice= myObj.nextLine();
     System.out.println("Use of Characters: " + thirdchoice);
 
@@ -34,15 +39,32 @@ static void passwordRules() { //Method for instructing rules for password
         passlength = myObj.nextInt();
         // block of code to be executed if the condition is true
       }
-   return firstchoice, secondchoice, thirdchoice, passlength;
+    String stringpasslength = Integer.toString(passlength);
+    String[] savedchoices= {firstchoice, secondchoice, thirdchoice, stringpasslength};
+    return savedchoices;
    // if(condition)
     // block of code to be executed if the condition is true
     
 
 }
-static void passwordGenerator() { //Method for Generating password
+public static String passwordGenerator(String firstchoice, String secondchoice, String thirdchoice, int passlength) { //Method for Generating password
+    StringBuilder sb = new StringBuilder();
+    Random random = new Random();
+    String yes = "Y";
+    String no = "N";
+    String candidateChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
 
-     
+    if(firstchoice.equals(yes) && secondchoice.equals(yes) && thirdchoice.equals(yes)){
+        
+        for (int i = 0; i < passlength; i++) {
+            sb.append(candidateChars.charAt(random.nextInt(candidateChars.length())));
+        }
+//System.out.println("End");
+}
+    
+   // else if (first choice == )
+   // use else if loops for other conditions for user inputs
+    return sb.toString();
 
 }
 
